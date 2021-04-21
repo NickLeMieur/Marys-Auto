344a9189bb70f79a9457d0eedfae37993d3913eb
Nick LeMieur <yj8244gd@go.minnstate.edu>
2021-04-21 16:45:07 -0500

    V 1.0.27

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const app = express();
 const exphbs = require('express-handlebars');
 //const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 const insecureHandlebars = allowInsecurePrototypeAccess(handlebars);
 const { removeAllListeners } = require('nodemon');
+const helpers = require('handlebars-helpers');
+const math = helpers.math();
 //port declaration
 const PORT = process.env.PORT || 1650;
 
@@ -34,6 +36,7 @@ require('./models/WorkOrder');
 const WorkOrder = mongoose.model('WorkOrder');
 require('./models/Reciept');
 const Reciept = mongoose.model('Reciept');
+const Both = mongoose.model('WorkOrder', 'Reciept');
 
 mongoose.connect(dbUrl,
   {
@@ -88,149 +91,156 @@ app.get('/order/viewWorkOrders', async (req, res) => {
 });
 
 app.get('/order/dailyProgress', async (req, res) => {
-    
-  await WorkOrder.find({}).lean().then(orders => {
+
+  await WorkOrder.find({}).lean().exec(function (err, orders) {
+    Reciept.find({}).lean().exec(function (err, reciept) {
       res.render('./order/dailyProgress', {
-        orders: orders.filter(function(myObj){
+        orders: orders.filter(function (myObj) {
+          return myObj.dateEntered >= new Date().setHours(0, 0, 0, 0)
+        }),
+        reciept: reciept.filter(function (myObj) {
           return myObj.dateEntered >= new Date().setHours(0,0,0,0)
         })
+      }
+  )
+
+  });
+
+  });
+});
+
+  app.post('/order/completeWorkOrder', async (req, res) => {
+    //console.log(req.body);
+
+    const order = {
+      fname: req.body.fname,
+      lname: req.body.lname,
+      vehModel: req.body.model,
+      vehMake: req.body.make,
+      vehYear: req.body.year,
+      phoneNumber: req.body.phoneNumber,
+      vehProblem: req.body.problems,
+      vehEstimate: req.body.cost
+    }
+    var ord = await WorkOrder(order).save().then(cOrder => {
+
+      console.log(cOrder);
+      //res.render('../order/viewWorkOrders');
+      res.redirect('../order/viewWorkOrders');
+      return res.status(201);
+    });
+
+
+  });
+  app.post('/order/deleteWorkOrder', async (req, res) => {
+
+    console.log(req.body.id);
+    await WorkOrder.deleteOne({ _id: req.body.id }).then(del => {
+      res.status(204).redirect('../order/viewWorkOrders');  //setting status to 204 (no content)
+    });
+  });
+
+
+
+  app.post('/order/doWorkOrder', async (req, res) => {
+
+    console.log(req.body);
+    var ord = await WorkOrder.findOne({ _id: req.body.id }).lean();
+    //console.log(ord);
+    //console.log(ord);
+    const date = new Date(ord.dateEntered);
+    console.log(date.toLocaleDateString());
+
+    console.log(ord);
+    //console.log(date.toString());
+    res.render('./order/doWorkOrder', {
+      id: req.body.id,
+      fname: ord.fname,
+      lname: ord.lname,
+      phoneNumber: ord.phoneNumber,
+      cost: ord.vehEstimate,
+      make: ord.vehMake,
+      model: ord.vehModel,
+      year: ord.vehYear,
+      problem: ord.vehProblem,
+      date: date
+    });
+    //console.log(ord);
+    return res.status(200);
+  });
+
+  app.post('/order/searchWorkOrder', async (req, res) => {
+
+    var orders = await WorkOrder.find({ fname: req.body.fname })
+      .lean().catch((err) => { console.log(err) });
+
+    res.render('./order/searchWorkOrder', {
+      ord: orders
+    });
+
+    return res.status(200);
+  });
+
+  app.post('/order/completeReciept', async (req, res) => {
+
+
+    var reciept = {
+      name: req.body.name,
+      make: req.body.make,
+      model: req.body.model,
+      cost: req.body.cost,
+      year: req.body.year,
+      hours: req.body.hours,
+      parts: req.body.parts,
+      problems: req.body.problems,
+      phoneNumber: req.body.phoneNumber
+    };
+    console.log(reciept);
+
+    await Reciept(reciept).save().then(print => {
+      res.render('./order/completeReciept', {
+        make: print.make,
+        model: print.model,
+        name: print.name,
+        cost: print.cost,
+        year: print.year,
+        parts: print.parts,
+        problems: print.problems,
+        phoneNumber: print.phoneNumber,
+        hours: print.hours
       });
-    
+    }).catch(err => {
+      console.log(err);
+      return res.status(500).json("Failed to Create Reciept");
     });
-});
 
-app.post('/order/completeWorkOrder', async (req, res) => {
-  //console.log(req.body);
 
-  const order = {
-    fname: req.body.fname,
-    lname: req.body.lname,
-    vehModel: req.body.model,
-    vehMake: req.body.make,
-    vehYear: req.body.year,
-    phoneNumber: req.body.phoneNumber,
-    vehProblem: req.body.problems,
-    vehEstimate: req.body.cost
-  }
-  var ord = await WorkOrder(order).save().then(cOrder => {
+    res.render('./order/completeReciept');
+    return res.status(200);
 
-    console.log(cOrder);
-    //res.render('../order/viewWorkOrders');
-    res.redirect('../order/viewWorkOrders');
-    return res.status(201);
+
   });
 
 
-});
-app.post('/order/deleteWorkOrder', async (req, res) => {
-
-  console.log(req.body.id);
-  await WorkOrder.deleteOne({ _id: req.body.id }).then(del => {
-    res.status(204).redirect('../order/viewWorkOrders');  //setting status to 204 (no content)
-  });
-});
 
 
 
-app.post('/order/doWorkOrder', async (req, res) => {
+  app.use('/order', orders);
 
-  console.log(req.body);
-  var ord = await WorkOrder.findOne({ _id: req.body.id }).lean();
-  //console.log(ord);
-  //console.log(ord);
-  const date = new Date(ord.dateEntered);
-  console.log(date.toLocaleDateString());
 
-  console.log(ord);
-  //console.log(date.toString());
-  res.render('./order/doWorkOrder', {
-    id: req.body.id,
-    fname: ord.fname,
-    lname: ord.lname,
-    phoneNumber: ord.phoneNumber,
-    cost: ord.vehEstimate,
-    make: ord.vehMake,
-    model: ord.vehModel,
-    year: ord.vehYear,
-    problem: ord.vehProblem,
-    date: date
-  });
-  //console.log(ord);
-  return res.status(200);
-});
 
-app.post('/order/searchWorkOrder', async (req, res) => {
 
-  var orders = await WorkOrder.find({ fname: req.body.fname })
-    .lean().catch((err) => { console.log(err) });
-
-  res.render('./order/searchWorkOrder', {
-    ord: orders
-  });
-
-  return res.status(200);
-});
-
-app.post('/order/completeReciept', async (req, res) => {
-
-  
-  var reciept = {
-    name: req.body.name,
-    make: req.body.make,
-    model: req.body.model,
-    cost: req.body.cost,
-    year: req.body.year,
-    hours: req.body.hours,
-    parts: req.body.parts,
-    problems: req.body.problems,
-    phoneNumber: req.body.phoneNumber
-  };
-  console.log(reciept);
-
-  await Reciept(reciept).save().then(print => {
-    res.render('./order/completeReciept', {
-      make: print.make,
-      model: print.model,
-      name: print.name,
-      cost: print.cost,
-      year: print.year,
-      parts: print.parts,
-      problems: print.problems,
-      phoneNumber: print.phoneNumber,
-      hours: print.hours
-    });
-  }).catch(err => {
-    console.log(err);
-    return res.status(500).json("Failed to Create Reciept");
+  //PAGE NOT FOUND ROUTE
+  app.get('*', (req, res) => {
+    res.status(404).send(
+      "404 Page Not Found! <a href='/'>Click to return to main</a><footer></footer>"
+    );
   });
 
 
-  res.render('./order/completeReciept');
-  return res.status(200);
+  //INIT SERVER
 
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 
-});
-
-
-
-
-
-app.use('/order', orders);
-
-
-
-
-//PAGE NOT FOUND ROUTE
-app.get('*', (req, res) => {
-  res.status(404).send(
-    "404 Page Not Found! <a href='/'>Click to return to main</a><footer></footer>"
-  );
-});
-
-
-//INIT SERVER
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-
-});
+  });
